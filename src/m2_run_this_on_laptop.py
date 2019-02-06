de28bfa00cb8e2869f12c5b5d27def18ebb753b7
jordanpj <jordanpj@rose-hulman.edu>
2019-02-06 08:49:31 -0500

    Put my names on the m2 files

--- src/m2_run_this_on_laptop.py
+++ src/m2_run_this_on_laptop.py
@@ -3,7 +3,7 @@
   Displays the Graphical User Interface (GUI) and communicates with the robot.
 
   Authors:  Your professors (for the framework)
-    and Parker Jordan
+    and Parker Jordan.
   Winter term, 2018-2019.
 """
 
