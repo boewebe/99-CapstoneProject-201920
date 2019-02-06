5528ece8bbecb2a8265f38bc788c584a310626e5
brownecw <brownecw@rose-hulman.edu>
2019-02-06 09:49:40 -0500

    updated test code for moving arm to position

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -26,7 +26,7 @@ def run_test_on_arm():
     robot.arm_and_claw.lower_arm()
     time.sleep(1)
     robot.arm_and_claw.calibrate_arm()
-    robot.arm_and_claw.move_arm_to_position(7.1)
+    robot.arm_and_claw.move_arm_to_position(7.1 * 360)
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
