e9c0c1634942a8a4d8f2989008cda4c09867e5c0
jordanpj <jordanpj@rose-hulman.edu>
2019-02-10 21:15:53 -0500

    Construced gui interface

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -356,7 +356,7 @@ def handle_move_arm_to_position(arm_position_entry, mqtt_sender):
       :type  mqtt_sender:        com.MqttClient
     """
     print("move_arm_to_position", arm_position_entry)
-    mqtt_sender.send_message("move_arm_to_position", arm_position_entry)
+    mqtt_sender.send_message("move_arm_to_position", [arm_position_entry])
 
 
 ###############################################################################
