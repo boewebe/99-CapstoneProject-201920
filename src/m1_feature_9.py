ea2aeb56fb0f0843362b615951bc3c7c97d1c84c
brownecw <brownecw@rose-hulman.edu>
2019-02-18 01:59:56 -0500

    fixed the led as approach so it times properly.

--- src/m1_feature_9.py
+++ src/m1_feature_9.py
@@ -4,23 +4,23 @@ import time
 def test_led_pickup(initial_led_rate, rate_of_led_increase):
     robot = rosebot.RoseBot()
     initial_distance = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
-    robot.drive_system.go(50, 50)
+     # robot.drive_system.go(50, 50)
     initial_led_rate = int(initial_led_rate)
     rate_of_led_increase = int(rate_of_led_increase)
 
     while True:
-        percent_distance = (initial_distance - robot.sensor_system.ir_proximity_sensor.get_distance_in_inches())
-        pause_time = (percent_distance) /(
-                    (initial_led_rate) + ((rate_of_led_increase) * (1 / 10) * (100 - percent_distance)))
+        percent_distance = (robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()) / (initial_distance)
+        pause_time = 3 *(percent_distance) / (((initial_led_rate) + ((rate_of_led_increase) * (1 - percent_distance))))
+        time.sleep(abs(pause_time))
         led_sequecne()
-        print(robot.sensor_system.ir_proximity_sensor.get_distance_in_inches())
-        time.sleep(pause_time)
+         #print(robot.sensor_system.ir_proximity_sensor.get_distance_in_inches())
+        print(pause_time)
         if robot.sensor_system.ir_proximity_sensor.get_distance_in_inches() <= 2:
             robot.drive_system.stop()
             break
 
     robot.arm_and_claw.raise_arm()
-
+    robot.arm_and_claw.calibrate_arm()  # this is for testing so it resets
 
 def led_sequecne():
     robot = rosebot.RoseBot()
